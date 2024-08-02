02d7c221331da6976cede7876ae2e4934d83f1af
Anthony Meza <64243783+anthony-meza@users.noreply.github.com>
2024-08-02 19:11:16 -0400

    Update poseidon_setup.sh

--- poseidon_setup.sh
+++ poseidon_setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-module load git 
 module load mambaforge 
+
+mamba create --name "test_enviornment" python=3.9
+mamba install -y --name "test_enviornment" -c conda-forge numpy pandas ipykernel pip-tools fastapi jupyterlab copernicusmarine
+
 
