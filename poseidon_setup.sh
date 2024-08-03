707b0f9257425a2bf39c9ed361684e3cc2034236
Anthony Meza <64243783+anthony-meza@users.noreply.github.com>
2024-08-03 11:35:45 -0400

    Update poseidon_setup.sh

--- poseidon_setup.sh
+++ poseidon_setup.sh
@@ -3,4 +3,17 @@
 curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
 bash Miniforge3-$(uname)-$(uname -m).sh
 
-mamba install -y --name "test_enviornment" -c conda-forge numpy pandas ipykernel pip-tools fastapi jupyterlab copernicusmarine
+#Step 1: Create Environment 
+mamba create -n hpc_tutorial
+
+#Step 2: Activate Environment 
+mamba activate hpc_tutorial
+
+#Step 3: Add packages to environment
+mamba install -y -c conda-forge numpy pandas ipykernel pip-tools fastapi jupyterlab copernicusmarine jupyter-resource-usage dask netCDF4 bottleneck xarray nodejs dask-labextension xesmf cmocean xhistogram scipy cartopy gsw geopy dask-jobqueue
+
+#Step 4: Add environment to Jupyter kernel
+python -m ipykernel install --user --name hpc_tutorial --display-name hpc_tutorial
+
+#Step 5: Fix Dask Dashboard ports
+sed -i 's|link: "/proxy/8787/status"|link: "/proxy/{port}/status"|' ~/.config/dask/jobqueue.yaml 
