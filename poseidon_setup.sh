5c8b2b66b3f55bfbba1bb6a84a29ba47470cc4c8
Anthony Meza <64243783+anthony-meza@users.noreply.github.com>
2024-08-02 20:11:49 -0400

    Update poseidon_setup.sh

--- poseidon_setup.sh
+++ poseidon_setup.sh
@@ -3,3 +3,4 @@
 curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
 bash Miniforge3-$(uname)-$(uname -m).sh
 
+mamba install -y --name "test_enviornment" -c conda-forge numpy pandas ipykernel pip-tools fastapi jupyterlab copernicusmarine
